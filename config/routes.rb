0175e53c5d044a1b3fefb9d4456777e13cc5903c
Amy Simmons <amy__simmons@hotmail.com>
2015-05-09 14:59:45 +1000

    events page set up

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get '/users/:id/admin-status/:status', to: 'users#update_admin_status', as: 'admin_status'
 
   get '/users/:user_id/vote/:vote_type', to: 'votes#create', as: 'vote'
+
+  get '/events', to: 'events#index', as: 'events'
 end
