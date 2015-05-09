db7297b7cf09309dbd62911cbe1b4a8dab9bfaa8
Amy Simmons <amy__simmons@hotmail.com>
2015-05-09 14:26:06 +1000

    updating seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,30 +3,24 @@ Admin.destroy_all
 Vote.destroy_all
 
 #create users
-User.create(name: 'Joanna Smith', email: 'Joanna_Smith@gmail.com', under_18: false, previous_attendant: true, track: 'Rails Girls Next', programming_experience: 'Some Ruby', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Sam Kelly', email: 'Sam_Kelly@gmail.com', under_18: true, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Rochelle Gainsford', email: 'Rochelle_Gainsford@gmail.com', under_18: false, previous_attendant: true, track: 'Rails Girls Next', programming_experience: 'Some Ruby', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Amy Wind', email: 'Amy_Wind@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Jennifer Embleton', email: 'Jennifer_Embleton@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Nicole Newman', email: 'Nicole_Newman@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Monica Osborn', email: 'Monica_Osborn@gmail.com', under_18: true, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Valentina Waters', email: 'Valentina_Waters@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'John Doe', email: 'John_Doe@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Caroline Rowland', email: 'Caroline_Rowland@gmail.com', under_18: false, previous_attendant: true, track: 'Rails Girls Next', programming_experience: 'Some Ruby', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Janet Kirk', email: 'Janet_Kirk@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Sophie Dorsey', email: 'Sophie_Dorsey@gmail.com', under_18: true, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Rose Simpson', email: 'Rose_Simpson@gmail.com', under_18: false, previous_attendant: true, track: 'Rails Girls Next', programming_experience: 'Some Ruby', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Liana Klein', email: 'Liana_Klein@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
-User.create(name: 'Melody Glenn', email: 'Melody_Glenn@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'applied')
+User.create(name: 'Joanna Smith', email: 'Joanna_Smith@gmail.com', under_18: false, previous_attendant: true, track: 'Rails Girls Next', programming_experience: 'Some Ruby', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'approved')
+User.create(name: 'Sam Kelly', email: 'Sam_Kelly@gmail.com', under_18: true, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'XS', user_status: '', admin_status: 'approved')
+User.create(name: 'Rochelle Gainsford', email: 'Rochelle_Gainsford@gmail.com', under_18: false, previous_attendant: true, track: 'Rails Girls Next', programming_experience: 'Some Ruby', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'approved')
+User.create(name: 'Amy Wind', email: 'Amy_Wind@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'approved')
+User.create(name: 'Jennifer Embleton', email: 'Jennifer_Embleton@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'approved')
+User.create(name: 'Nicole Newman', email: 'Nicole_Newman@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'L', user_status: '', admin_status: 'rejected')
+User.create(name: 'Monica Osborn', email: 'Monica_Osborn@gmail.com', under_18: true, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'deferred')
+User.create(name: 'Valentina Waters', email: 'Valentina_Waters@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'rejected')
+User.create(name: 'John Doe', email: 'John_Doe@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'XS', user_status: '', admin_status: 'approved')
+User.create(name: 'Caroline Rowland', email: 'Caroline_Rowland@gmail.com', under_18: false, previous_attendant: true, track: 'Rails Girls Next', programming_experience: 'Some Ruby', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'approved')
+User.create(name: 'Janet Kirk', email: 'Janet_Kirk@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'M', user_status: '', admin_status: 'approved')
+User.create(name: 'Sophie Dorsey', email: 'Sophie_Dorsey@gmail.com', under_18: true, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'approved')
+User.create(name: 'Rose Simpson', email: 'Rose_Simpson@gmail.com', under_18: false, previous_attendant: true, track: 'Rails Girls Next', programming_experience: 'Some Ruby', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'deferred')
+User.create(name: 'Liana Klein', email: 'Liana_Klein@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'deferred')
+User.create(name: 'Melody Glenn', email: 'Melody_Glenn@gmail.com', under_18: false, previous_attendant: false, track: 'Rails Girls', programming_experience: 'none', reason: 'I want to learn to program', tshirt_size: 'S', user_status: '', admin_status: 'approved')
 
 #create admins
 Admin.create(username: 'Amy Simmons', email: 'amysimmons.abc@gmail.com')
 Admin.create(username: 'Lucy Bain', email: 'lucybain@gmail.com')
 Admin.create(username: 'Angie', email: 'angie@gmail.com')
-
-#create votes
-Vote.create(admin_id: 4, user_id: 106, vote_type: 'up')
-Vote.create(admin_id: 5, user_id: 107, vote_type: 'up')
-Vote.create(admin_id: 6, user_id: 108, vote_type: 'down')
-
 
