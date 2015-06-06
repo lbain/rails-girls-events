8dee83f774ff407b573849ca442f098ac6357ebd
Lucy Bain <lucybain@gmail.com>
2015-06-06 16:27:27 +1000

    users should be able to see thank you page

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   devise_for :admins
 
   authenticate :admin do
-    resources :users, except: [:edit, :destroy] do
+    resources :users, except: [:create, :edit, :destroy] do
       resources :votes, only: [:create]
     end
     get '/events', to: 'events#index', as: 'events'
   end
 
-  # Left separate from update since it's not for admin
+  resources :users, only: [:create]
   get '/users/:id/user-status/:status', to: 'users#update_user_status', as: 'user_status'
 
 end
