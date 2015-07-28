0691aeed5b3ebdd23abf3ecc16ef714c2ec1dc2e
Lucy Bain <lucybain@gmail.com>
2015-07-28 21:50:37 +1000

    Show track for users and make html a bit easier to update

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  def displayed_attributes
+    { name: "Name",
+      email: "Email",
+      gender: "Gender",
+      under_18: "Age",
+      track: "Track",
+      previous_attendance: "Rails Girls experience",
+      programming_experience: "Programming experience",
+      reason: "Reason for applying" }
+  end
 end
