0eed3c2bb8954eb73533a492139aec50225ff89b
tracymu <tracy.musung@moomumedia.com>
2016-02-23 21:22:06 +1100

    Add previous applications

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,10 @@ module UsersHelper
     { name: "Name",
       email: "Email" }
   end
+
+  def previous_application_attributes
+    { track: "Track",
+      admin_status: "Admin status",
+      user_status: "User status" }
+  end
 end
